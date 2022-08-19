d92a40481e5bca6354d62fa1991912857dde6232
Quang Bui <buiphuquang2412@gmail.com>
2022-08-19 20:29:28 +0700

    Add amplify auth functions

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react'
 import Logo from '../youtube.svg'
 import { Grid, Button, TextField } from '@mui/material'
+import { Auth } from 'aws-amplify'
 
 import { styled } from '@mui/material/styles'
 
@@ -12,9 +14,35 @@ const StyledTextField = styled(TextField)(({ theme }) => ({
 
 const StyledButton = styled(Button)(({ theme }) => ({
   color: '#fff',
+  marginRight: 10,
 }))
 
-const Header = () => {
+const Header: React.FC = () => {
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
+  const login = async () => {
+    try {
+      const { user } = await Auth.signUp({
+        username: email,
+        password,
+      })
+    } catch (error: any) {
+      console.log(error.message)
+    }
+  }
+
+  const signup = async () => {
+    try {
+      const { user } = await Auth.signIn({
+        username: email,
+        password,
+      })
+    } catch (error: any) {
+      console.log(error.message)
+    }
+  }
+
   return (
     <Grid container spacing={2}>
       <Grid item xs={1}>
@@ -22,14 +50,27 @@ const Header = () => {
       </Grid>
       <Grid item xs={11}>
         <Grid container justifyContent='flex-end' component='form' padding={1}>
-          <StyledTextField size='small' id='outlined-required' label='Email' />
           <StyledTextField
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            size='small'
+            id='outlined-required'
+            label='Email'
+          />
+          <StyledTextField
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
             size='small'
             id='outlined-password-input'
             label='Password'
             type='password'
           />
-          <StyledButton variant='contained'>Log in</StyledButton>
+          <StyledButton onClick={login} variant='contained'>
+            Log in
+          </StyledButton>
+          <StyledButton onClick={signup} variant='contained'>
+            Sign up
+          </StyledButton>
         </Grid>
       </Grid>
     </Grid>
