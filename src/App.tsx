7a6f4bd42092ef28f4d10daa345eb69a211ee73b
Quang Bui <buiphuquang2412@gmail.com>
2022-08-20 20:42:22 +0700

    Add auth check to route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import { Route, Routes } from 'react-router-dom'
+import React from 'react'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import Home from './components/Home'
 import Share from './components/Share'
+import { useUserContext } from './context'
+
+const AuthRoute: React.FC<{ component: React.FC }> = ({ component: C }) => {
+  const { userEmail } = useUserContext()
+  return userEmail ? <C /> : <Navigate to='/' />
+}
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/share' element={<Share />} />
+      <Route path='/share' element={<AuthRoute component={Share} />} />
     </Routes>
   )
 }
